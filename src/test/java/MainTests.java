936ce5ac87b5797fc34e4cd0312031c83ca2709d
Your Name <you@example.com>
2017-01-19 10:47:45 +0000

    Add file to tests

---
+++ src/test/java/MainTests.java
@@ -0,0 +1,15 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Created by ape12 on 19/01/2017.
+ */
+
+
+public class MainTests {
+
+    @Test
+    public void testThatAlwaysFails(){
+        Assert.assertEquals(1,2);
+    }
+}
